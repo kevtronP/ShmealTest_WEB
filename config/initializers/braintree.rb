f220a1610d337aab0c970e44e5acaa8423e4352f
Justin Hamer <hamerjm@Justins-iMac.local>
2016-10-28 22:24:29 -0500

    ShmealApp_v1

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,5 +1,5 @@
 Braintree::Configuration.environment = :sandbox
 Braintree::Configuration.logger = Logger.new('log/braintree.log')
-Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
-Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
-Braintree::Configuration.private_key = ENV['BRAINTREE_PRIVATE_KEY']
+Braintree::Configuration.merchant_id = p9gqz47jdpzqhk68
+Braintree::Configuration.public_key = nnc22kcws697bqrw
+Braintree::Configuration.private_key = 13fef4c2af0b5d2ad368d54ce0ac0524
