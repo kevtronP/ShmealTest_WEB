2b5f30d3320bd1ecfd22944ff635aba4e0827fca
Justin Hamer <hamerjm@Justins-iMac.local>
2017-03-14 18:13:19 -0500

    ShmealApp_v1 payments

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,4 @@
-Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.environment = :production
 Braintree::Configuration.logger = Logger.new('log/braintree.log')
 Braintree::Configuration.merchant_id = '9vfbk4f6tk55ww6v'
 Braintree::Configuration.public_key = 'kpgbhvtb87nq32qy'
