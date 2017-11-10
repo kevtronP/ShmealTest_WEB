252d4a0c70180d90eb23b28723459fa7082d2cc4
Justin Hamer <hamerjm@gmail.com>
2017-11-10 16:12:23 -0600

    Shmeal routes shmshmealstatuses removing index

--- config/routes.rb
+++ config/routes.rb
@@ -21,9 +21,10 @@ Rails.application.routes.draw do
   resources :shmorders
   resources :shmconversations
   resources :shmeals do
-    resources :shmshmealstatuses, only: [:show, :index, :new, :create]
+    #resources :shmshmealstatuses, only: [:show, :new, :create]
+    resources :shmshmealstatuses, except: [:index]
   end
-  resources :shmshmealstatuses, only: [:edit, :update, :destroy]
+  #resources :shmshmealstatuses, only: [:edit, :update, :destroy]
   resources :users
   resources :transactions, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
