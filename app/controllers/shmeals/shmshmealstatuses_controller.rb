c9757c71e15adce7f903b4e1eb24c0172a3ea396
Justin Hamer <hamerjm@gmail.com>
2017-11-11 00:34:04 -0600

    ShmealTest shmealID instead of shmeal_id

--- app/controllers/shmeals/shmshmealstatuses_controller.rb
+++ app/controllers/shmeals/shmshmealstatuses_controller.rb
@@ -4,7 +4,7 @@ class Shmeals::ShmshmealstatusesController < ApplicationController
   # GET /shmshmealstatuses
   def index
     #@shmshmealstatuses = Shmshmealstatus.all
-    @shmeal = Shmeal.find(params[:shmeal_id])
+    @shmeal = Shmeal.find(params[:shmealID])
     @shmshmealstatuses = @shmeal.shmshmealstatuses
 
     render json: @shmshmealstatuses
