994008f726411da9f08c9f3950eea1155d6ccd21
Justin Hamer <hamerjm@gmail.com>
2018-02-19 21:07:03 -0600

    ShmealTest_v1 adding freeShmeals to test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:userEmail, :userPhoneNumber, :userName, :lastName, :userDate)
+      params.require(:user).permit(:userEmail, :userPhoneNumber, :userName, :lastName, :userDate, :freeShmeals)
     end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :userEmail, :userPhoneNumber, :userName, :lastName, :userDate
+  attributes :id, :userEmail, :userPhoneNumber, :userName, :lastName, :userDate, freeShmeals
 end
