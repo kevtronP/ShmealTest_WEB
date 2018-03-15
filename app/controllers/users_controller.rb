2fc45fdca8bb62bb5fa8b01543fd140f86f00f49
Justin Hamer <hamerjm@gmail.com>
2018-03-15 17:07:11 -0500

    ShmealTest_v1 back

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @shmorders = @user.shmorders
     @shmuserattributes = @user.shmuserattributes
     @shmuserstatuses = @user.shmuserstatuses
-    render json: @user, @user.menuitems
+    render json: @user
   end
 
   # POST /users
