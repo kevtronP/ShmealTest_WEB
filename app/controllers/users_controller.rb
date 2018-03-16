4b6f260bf83b3ceee5c3fbad4ea6f7fffe3661ab
Justin Hamer <hamerjm@gmail.com>
2018-03-16 17:28:34 -0500

    ShmealTest_v1 trying to get rid of nil each error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,6 +63,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:userEmail, :userPhoneNumber, :userName, :lastName, :userDate, :freeShmeals, menuitems: [:mealName, :userID, :menuItemDate])
+      params.require(:user).permit(:userEmail, :userPhoneNumber, :userName, :lastName, :userDate, :freeShmeals)
     end
 end
