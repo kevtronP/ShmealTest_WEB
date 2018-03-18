a3b530c63e1310ca00c477a655a7d729dff184dd
Justin Hamer <hamerjm@gmail.com>
2018-03-17 22:50:14 -0500

    ShmealTest_v1 back to workign on serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :userEmail, :userPhoneNumber, :userName, :lastName, :userDate, :freeShmeals
-  #has_many :menuitems
+  has_many :menuitems
 end
