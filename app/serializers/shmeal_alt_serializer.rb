340715362b08ac604dccad517daf195acc90f2df
Justin Hamer <hamerjm@gmail.com>
2018-03-23 15:55:38 -0500

    ShmealTest_v1 tinkering with shmeal_alt_serializer

--- app/serializers/shmeal_alt_serializer.rb
+++ app/serializers/shmeal_alt_serializer.rb
@@ -1,5 +1,12 @@
 class ShmealAltSerializer < ShmealSerializer
   has_many :shmshmealstatuses
   has_many :shmshmealattributes
-  belongs_to :menuitem, has_many :shmshmealattributes
+  belongs_to :menuitem
+
+  def shmealMenuItems
+
+    Shmshmealattribute.where(:menuItemID => self.menuitem_id)
+
+  end
+
 end
