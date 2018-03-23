460def52ef429b48f00c6f3a4446ed71573dbc17
Justin Hamer <hamerjm@gmail.com>
2018-03-23 15:08:51 -0500

    ShmealTest_v1 trying shmeal alt serializer without inheritance

--- app/serializers/shmeal_alt_serializer.rb
+++ app/serializers/shmeal_alt_serializer.rb
@@ -1,4 +1,5 @@
-class ShmealAltSerializer < ShmealSerializer
+class ShmealAltSerializer < ActiveModel::Serializer
+  attributes :id, :shmealDayDate, :menuItemID
   has_many :shmshmealstatuses
   has_many :shmshmealattributes
   belongs_to :menuitem
