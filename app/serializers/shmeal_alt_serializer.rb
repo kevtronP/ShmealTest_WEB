e7f53530335c43afd4b87909319e6824dcf6de5a
Justin Hamer <hamerjm@gmail.com>
2018-03-28 20:47:46 -0500

    ShmealTest_v1 trying serializer for menuitem again

--- app/serializers/shmeal_alt_serializer.rb
+++ app/serializers/shmeal_alt_serializer.rb
@@ -1,6 +1,6 @@
 class ShmealAltSerializer < ShmealSerializer
   has_many :shmshmealstatuses
   has_many :shmshmealattributes
-  belongs_to :menuitem
+  belongs_to :menuitem, serializer: MenuitemAltSerializer
 
 end
