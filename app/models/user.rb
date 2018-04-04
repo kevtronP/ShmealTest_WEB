f93cf8db7f872d8c20e370ad8541200a7137624e
Justin Hamer <hamerjm@gmail.com>
2018-04-04 14:16:16 -0500

    ShmealTest_v1 adding shmcookstatuses to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :shmaddresses
   has_many :shmconversations
   has_many :shmcooks
+  has_many :shmcookstatuses, through: :shmcooks
   has_many :shmcustomerpmtmethods
   has_many :shmfundings
   has_many :shmorders
