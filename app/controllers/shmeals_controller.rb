2981108f49e241b18732702e9c55a158d25d1d2c
Justin Hamer <hamerjm@gmail.com>
2018-04-09 14:35:36 -0500

    ShmealTest_v1 adding user and userattributes to shmeals call

--- app/controllers/shmeals_controller.rb
+++ app/controllers/shmeals_controller.rb
@@ -9,7 +9,7 @@ class ShmealsController < ApplicationController
 
     @shmeals = Shmeal.where({shmealDayDate: Date.today..Date.today.next_month})
 
-    render json: @shmeals, each_serializer: ShmealAltSerializer, include: 'menuitem,shmshmealattributes,shmshmealstatuses,menuitem.shmshmealattributes'
+    render json: @shmeals, each_serializer: ShmealAltSerializer, include: 'menuitem,shmshmealattributes,shmshmealstatuses,menuitem.shmshmealattributes,menuitem.user,menuitem.user.shmuserattributes'
   end
 
   #def future
