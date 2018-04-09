79c276899a9a56e638502070393949bfb4c87e1f
Justin Hamer <hamerjm@gmail.com>
2018-04-09 16:47:49 -0500

    ShmealTest_v1 using user_altserialialzer for menuitem.user

--- app/serializers/menuitem_alt_serializer.rb
+++ app/serializers/menuitem_alt_serializer.rb
@@ -2,6 +2,5 @@ class MenuitemAltSerializer < MenuitemSerializer
   has_many :shmeals
   has_many :shmmenuitemstatuses
   has_many :shmshmealattributes
-  belongs_to :user
-  has_many :shmuserattributes, through: :user
+  belongs_to :user, serializer: UserAltSerializer
 end
