33772cee877c326ec8f689f11ba85efbb106a5ff
Kevin Mercado <kmerc5187@gmail.com>
2018-10-04 14:47:39 -0500

    removed webpacker fonfig check

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,16 +1,13 @@
 Rails.application.configure do
-  # Verifies that versions and hashed value of the package contents in the project's package.json
-config.webpacker.check_yarn_integrity = true
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports.
   config.consider_all_requests_local = true
